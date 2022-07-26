fc275472d029b9f3ab824287fe4811bb9bc734c8
jihwooon <jihwooon@gmail.com>
2022-07-27 01:18:08 +0900

    REDEME.md Togger 기능 추가

---
+++ 3-interface/app/src/test/java/core/SquareSequence.java
@@ -0,0 +1,16 @@
+package core;
+
+public class SquareSequence implements IntSequence {
+    private int i;
+
+    @Override
+    public boolean hasNext() {
+        return true;
+    }
+
+    @Override
+    public int next() {
+        i++;
+        return i * i;
+    }
+}
