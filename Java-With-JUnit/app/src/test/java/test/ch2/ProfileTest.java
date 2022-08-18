f02aaee6903f2cd22dd3074841840f155b613f8e
jihwooon <jihwooon@gmail.com>
2022-08-18 17:07:37 +0900

    테스트 코드 리팩토링

--- Java-With-JUnit/app/src/test/java/test/ch2/ProfileTest.java
+++ Java-With-JUnit/app/src/test/java/test/ch2/ProfileTest.java
@@ -20,12 +20,8 @@ public class ProfileTest {
 
     @Test
     public void matchAnswersFalseWhenMustMatchCriteriaNotMet() {
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-        Answer criteriaAnswer = new Answer(question, Bool.TRUE);
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.MustMatch);
-
-        criteria.add(criterion);
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.MustMatch));
 
         boolean matches = profile.matches(criteria);
 
@@ -35,12 +31,8 @@ public class ProfileTest {
 
     @Test
     public void matchAnswersTrueForAnyDontCareCriteria() {
-        Answer profileAnswer = new Answer(question, Bool.FALSE);
-        profile.add(profileAnswer);
-        Answer criteriaAnswer = new Answer(question, Bool.TRUE);;
-        Criterion criterion = new Criterion(criteriaAnswer, Weight.DontCare);
-
-        criteria.add(criterion);
+        profile.add(new Answer(question, Bool.FALSE));
+        criteria.add(new Criterion(new Answer(question, Bool.TRUE), Weight.DontCare));
 
         boolean matches = profile.matches(criteria);
 
