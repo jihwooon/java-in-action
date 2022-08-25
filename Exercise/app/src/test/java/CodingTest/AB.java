5378f8821a43bda0694ce50f2dfecc2384894883
jihwooon <jihwooon@gmail.com>
2022-08-25 15:24:15 +0900

    코딩 테스트 문제 풀이

--- Exercise/app/src/test/java/CodingTest/AB.java
+++ Exercise/app/src/test/java/CodingTest/AB.java
@@ -1,10 +1,19 @@
-package example;
+package CodingTest;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+/**
+ * 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+ *
+ * 입력은 여러 개의 테스트 케이스로 이루어져 있다.
+ * 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+ * 입력의 마지막에는 0 두 개가 들어온다.
+ *
+ * 각 테스트 케이스마다 A+B를 출력한다.
+ * */
+public class AB {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -19,4 +28,3 @@ public class Main {
         System.out.println(A+B);
     }
 }
-
--- Exercise/app/src/test/java/CodingTest/Number_Sort.java
+++ Exercise/app/src/test/java/CodingTest/Number_Sort.java
@@ -1,22 +1,22 @@
-package example;
+package CodingTest;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashMap;
 
-public class Main {
+public class Number_Sort {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int A = Integer.parseInt(br.readLine());
-        int B = Integer.parseInt(br.readLine());
 
-        if(A == 0 && B == 0) {
-            System.exit(0);
-        }
+        // 첫번째 줄에 수의 개수를 입력한다.
+        System.out.println(A);
 
-        System.out.println(A+B);
+        // 둘 째 줄부터 N개의 줄에는 수 주어진다. 수는 중복이 없다.
+
+
     }
 }
-
