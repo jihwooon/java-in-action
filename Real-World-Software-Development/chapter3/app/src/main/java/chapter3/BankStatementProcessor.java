d8bd001eb26d2d2e1df202928f8cd53141ea50b4
jihwooon <jihwooon@gmail.com>
2023-06-13 09:20:16 +0900

    BankStatementProcessor에 내역 찾기 기능을 추가하라
    특정 금액 이상 및 특정 월에 따른 내역 찾기 기능을 추가했습니다.
    이 메서드들의 문제는 새로운 기능이 추가 될 때마다 중복 문제가 발생합니다 때문에 로직을 변경 할 때마다 여러 조건을 변경해야 하는 문제가 있습니다.

--- Real-World-Software-Development/chapter3/app/src/main/java/chapter3/BankStatementProcessor.java
+++ Real-World-Software-Development/chapter3/app/src/main/java/chapter3/BankStatementProcessor.java
@@ -38,4 +38,26 @@ public class BankStatementProcessor {
         }
         return bankTransactionsInMonth;
     }
+
+    // 특정 금액 이상의 은행 거래 내역 찾기
+    public List<BankTransaction> findTransactionsGreaterThanEqual(final int amount) {
+        ArrayList<BankTransaction> result = new ArrayList<>();
+        for (BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getAmount() > amount) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
+
+    // 특정 월의 입출금 내역 찾기
+    public List<BankTransaction> findTransactionsInMonth(final Month month) {
+        ArrayList<BankTransaction> result = new ArrayList<>();
+        for (BankTransaction bankTransaction : bankTransactions) {
+            if (bankTransaction.getDate().getMonth() == month) {
+                result.add(bankTransaction);
+            }
+        }
+        return result;
+    }
 }
