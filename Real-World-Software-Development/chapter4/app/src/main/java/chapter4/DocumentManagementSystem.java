8b2e8b7f5949864bc81781f99ce4670978e2a570
jihwooon <jihwooon@gmail.com>
2023-06-14 10:18:20 +0900

    importFile 메서드를 구현하라
    리스코프 치환 원칙을 적용하기 위해서 메서드를 생성했습니다.
    
    리스코프 치환 원칙은 자식 클래스는 보무로부터 물려받은 행동을 유지해야
    한다의 원칙으로 형식은 클래스나 인터페이스를 의미하고 하위형식은 두
    형식의 부모 자식관계를 말합니다.

--- Real-World-Software-Development/chapter4/app/src/main/java/chapter4/DocumentManagementSystem.java
+++ Real-World-Software-Development/chapter4/app/src/main/java/chapter4/DocumentManagementSystem.java
@@ -1,12 +1,40 @@
 package chapter4;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class DocumentManagementSystem {
+    private final List<Document> documents = new ArrayList<>();
     private final Map<String, Importer> extensionToImporter = new HashMap<>();
 
     public DocumentManagementSystem() {
         extensionToImporter.put("jpg", new ImageImporter());
+    }
+
+    public void importFile(final String path) throws IOException {
+        final File file = new File(path);
+        if (!file.exists()) {
+            throw new FileNotFoundException(path);
+        }
+        final int separatorIndex = path.lastIndexOf(".");
+        if (separatorIndex != -1) {
+            if (separatorIndex == path.length()) {
+                throw new UnknownFileTypeException("No extension found file: " + path);
+            }
+            final String extension = path.substring(separatorIndex + 1);
+            Importer importer = extensionToImporter.get(extension);
+            if (importer == null) {
+                throw new UnknownFileTypeException("For file: " + path);
+            }
+            Document document = importer.importFile(file);
+            documents.add(document);
+        } else {
+            throw new UnknownFileTypeException("No extension found for file: " + path);
+        }
     }
 }
---
+++ Real-World-Software-Development/chapter4/app/src/main/java/chapter4/UnknownFileTypeException.java
@@ -0,0 +1,7 @@
+package chapter4;
+
+public class UnknownFileTypeException extends RuntimeException {
+    public UnknownFileTypeException(String message) {
+        super(message);
+    }
+}
