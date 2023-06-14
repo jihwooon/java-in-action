a832d46b4b71f0e7fe350b0a47bac9e921f37fc2
jihwooon <jihwooon@gmail.com>
2023-06-14 09:44:15 +0900

    속성명을 가리키는 Attributes 클래스를 성성하라
    상수로 정의 Attributes 클래스를 생성했습니다.

---
+++ Real-World-Software-Development/chapter4/app/src/main/java/chapter4/Attributes.java
@@ -0,0 +1,9 @@
+package chapter4;
+
+public final class Attributes {
+    public static final String PATH = "path";
+    public static final String WIDTH = "width";
+    public static final String HEIGHT = "height";
+    public static final String TYPE = "type";
+
+}
