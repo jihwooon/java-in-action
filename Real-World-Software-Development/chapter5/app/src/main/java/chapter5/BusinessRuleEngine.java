2f97b45eb154e414d55dadba9d3465457bbc1e6e
jihwooon <jihwooon@gmail.com>
2023-06-16 13:03:04 +0900

    요구 사항이 확정 되지 않은 BusinessRuleEngine 클래스를 생성하라
    아직 메서드가 구현되지 않아 모든 메서드를 UnsupportedOperationException 예외로 던집니다.

---
+++ Real-World-Software-Development/chapter5/app/src/main/java/chapter5/BusinessRuleEngine.java
@@ -0,0 +1,16 @@
+package chapter5;
+
+public class BusinessRuleEngine {
+
+    public void addAction(final Action action) {
+        throw new UnsupportedOperationException();
+    }
+
+    public int count() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void run() {
+        throw new UnsupportedOperationException();
+    }
+}
