89dcc6700fe52df8cb1b947e6e811a567d8d6d22
jihwooon <jihwooon@gmail.com>
2023-06-16 14:23:26 +0900

    BusinessRuleEngine 테스트 코드를 리팩터링 하라
    BDD 스타일의 테스트 형식으로 변경했습니다.

--- Real-World-Software-Development/chapter5/app/src/test/java/chapter5/BusinessRuleEngineTest.java
+++ Real-World-Software-Development/chapter5/app/src/test/java/chapter5/BusinessRuleEngineTest.java
@@ -1,24 +1,32 @@
 package chapter5;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
+@DisplayName("BusinessRuleEngine 클래스")
 class BusinessRuleEngineTest {
 
-    @Test
-    void shouldHaveNoRulesInitially() {
-        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
+    private BusinessRuleEngine businessRuleEngine;
 
+    @BeforeEach
+    public void setUp() {
+        this.businessRuleEngine = new BusinessRuleEngine();
+    }
+
+    @Test
+    @DisplayName("businessRuleEngine에 아무것도 생성이 되어 있지 않으면 count 횟수가 0이 나온다.")
+    void shouldHaveNoRulesInitially() {
         assertThat(businessRuleEngine.count()).isEqualTo(0);
     }
 
     @Test
+    @DisplayName("액션이 두 개 이상 추가되면 addAction의 갯수만큼 count를 센다.")
     void shouldAddTwoActions() {
-        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
-
         businessRuleEngine.addAction(() -> {});
         businessRuleEngine.addAction(() -> {});
 
@@ -26,8 +34,8 @@ class BusinessRuleEngineTest {
     }
 
     @Test
+    @DisplayName("businessRuleEngine의 run 메서드를 검증한다.")
     public void shouldExecuteOneAction() {
-        final BusinessRuleEngine businessRuleEngine = new BusinessRuleEngine();
         Action mockAction = mock(Action.class);
 
         businessRuleEngine.addAction(mockAction);
