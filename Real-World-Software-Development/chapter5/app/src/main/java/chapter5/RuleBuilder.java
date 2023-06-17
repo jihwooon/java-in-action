af9deba922b63fe17d923ed7f53055578103705e
jihwooon <jihwooon@gmail.com>
2023-06-17 12:56:18 +0900

    빌더 패턴을 생성하라

---
+++ Real-World-Software-Development/chapter5/app/src/main/java/chapter5/RuleBuilder.java
@@ -0,0 +1,20 @@
+package chapter5;
+
+public class RuleBuilder {
+    private Condition condition;
+    private Action action;
+
+    public RuleBuilder when(final Condition condition) {
+        this.condition = condition;
+        return this;
+    }
+
+    public RuleBuilder then(final Action action) {
+        this.action = action;
+        return this;
+    }
+
+    public Rule createRule() {
+        return new DefaultRule(condition, action);
+    }
+}
