20a4f88f5defcf77afa3f3e52aaefae5bf8c77af
jihwooon <jihwooon@gmail.com>
2023-06-18 12:52:55 +0900

    팔로워와 트웃의 테스트를 통과하라
    중복과 성공의 테스트를 통과했습니다.

--- Real-World-Software-Development/chapter6/app/src/test/java/chapter6/TwootrTest.java
+++ Real-World-Software-Development/chapter6/app/src/test/java/chapter6/TwootrTest.java
@@ -1,6 +1,10 @@
 package chapter6;
 
+import static chapter6.FollowStatus.ALREADY_FOLLOWING;
+import static chapter6.FollowStatus.SUCCESS;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 import java.util.Optional;
@@ -13,11 +17,14 @@ class TwootrTest {
   private UserRepository userRepository = mock(UserRepository.class);
 
   private Twootr twootr;
+  private User user;
   private SenderEndPoint endPoint;
 
   @BeforeEach
   public void setUp() {
+    user = new User(TestData.USER_ID, TestData.PASSWORD_BYTES, TestData.SALT);
     twootr = new Twootr(userRepository);
+    endPoint = new SenderEndPoint(user, twootr);
   }
 
   @Test
@@ -25,5 +32,31 @@ class TwootrTest {
     Optional<SenderEndPoint> endPoint = twootr.onLogon(TestData.USER_ID, "bad password",
         receiverEndPoint);
     assertFalse(endPoint.isPresent());
+  }
+
+  @Test
+  public void shouldFollowValidUser() {
+    FollowStatus followStatus = endPoint.onFollow(TestData.OTHER_USER_ID);
+
+    assertThat(followStatus).isEqualTo(SUCCESS);
+  }
+
+  @Test
+  public void shouldNotDuplicateFollowValidUser() {
+    endPoint.onFollow(TestData.USER_ID);
+    FollowStatus followStatus = endPoint.onFollow(TestData.USER_ID);
+
+    assertThat(followStatus).isEqualTo(ALREADY_FOLLOWING);
+  }
+
+  private void logon() {
+    this.endPoint = logon(TestData.USER_ID, receiverEndPoint);
+  }
+
+  private SenderEndPoint logon(final String userId, final ReceiverEndPoint receiverEndPoint) {
+    final Optional<SenderEndPoint> endPoint = twootr.onLogon(userId, TestData.PASSWORD,
+        receiverEndPoint);
+    assertTrue(endPoint.isPresent(), "Failed to logon");
+    return endPoint.get();
   }
 }
