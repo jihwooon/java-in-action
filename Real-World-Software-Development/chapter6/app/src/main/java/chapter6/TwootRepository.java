4af1893092b139701825b2745a6b989f466e918c
jihwooon <jihwooon@gmail.com>
2023-06-18 23:20:04 +0900

    영구 저장과 저장소 패턴을 구현하라

--- Real-World-Software-Development/chapter6/app/src/main/java/chapter6/TwootRepository.java
+++ Real-World-Software-Development/chapter6/app/src/main/java/chapter6/TwootRepository.java
@@ -1,12 +1,17 @@
 package chapter6;
 
 import java.util.Optional;
+import java.util.function.Consumer;
 
 public interface TwootRepository {
 
   Twoot add(String id, String userId, String content);
 
   Optional<Twoot> get(String id);
+
+  void delete(Twoot twoot);
+
+  void query(TwootQuery twootQuery, Consumer<Twoot> callback);
 
   void clear();
 }
--- Real-World-Software-Development/chapter6/app/src/main/java/chapter6/UserRepository.java
+++ Real-World-Software-Development/chapter6/app/src/main/java/chapter6/UserRepository.java
@@ -4,6 +4,13 @@ import java.util.Optional;
 
 public interface UserRepository extends AutoCloseable {
 
+  boolean add(User user);
+
   Optional<User> get(String userId);
 
+  void update(User user);
+
+  void clear();
+
+  FollowStatus follow(User follower, User userToFollow);
 }
