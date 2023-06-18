61090841cd3ddac56aef1281a0ca906b12f3eec3
jihwooon <jihwooon@gmail.com>
2023-06-19 00:10:22 +0900

    람다 표현식으로 구현하라

--- Real-World-Software-Development/chapter6/app/src/main/java/chapter6/PrintingEndPointSnippets.java
+++ Real-World-Software-Development/chapter6/app/src/main/java/chapter6/PrintingEndPointSnippets.java
@@ -1,2 +1,22 @@
-package chapter6;public class PrintingEndPointSnippets {
+package chapter6;
+
+import chapter6.domain.Twoot;
+
+public class PrintingEndPointSnippets {
+
+  public static void main(String[] args) {
+
+    final ReceiverEndPoint anonymousClass = new ReceiverEndPoint() {
+
+      @Override
+      public void onTwoot(Twoot twoot) {
+        System.out.println(twoot.getSenderId() + ": " + twoot.getContent());
+      }
+    };
+
+    final ReceiverEndPoint lambda = twoot -> System.out.println(
+        twoot.getSenderId() + ": " + twoot.getContent());
+
+  }
+
 }
